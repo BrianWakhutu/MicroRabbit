034b670835c3a5bda2675a167fa3dc0d6226f65b
Brian Wakhutu <brian.wakhutu@gmail.com>
2019-07-03 14:15:20 +0300

    Initial Microservice

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,4 +1,9 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -10,7 +15,15 @@ namespace MicroRabbit.Infra.IoC
     public class DependencyContainer
     {
         public static void RegisterServices(IServiceCollection services) {
+
+            //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+            ///Applicaition Services
+            services.AddTransient<IAccountService, AccountService>();
+
+            //DAta
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
